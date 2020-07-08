425e1b15990d2ab6948e2e8e73822b50561a8933
fyl080801 <fyl080801@hotmail.com>
2020-07-09 02:32:30 +0800

    feat: condition

--- package/features/provider/condition.js
+++ package/features/provider/condition.js
@@ -7,7 +7,7 @@ feature.provider(function() {
 
     const defined = Object.getOwnPropertyDescriptor(field, "condition");
 
-    if (defined === undefined || condition === undefined) {
+    if (defined === undefined) {
       return;
     }
 
