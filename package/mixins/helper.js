3b1f529a4928e171dd075d0c37a609f2f546f31c
fengyuanliang3 <fengyuanliang2@jd.com>
2020-08-17 14:51:43 +0800

    fix: $deepSet 死循环问题

--- package/mixins/helper.js
+++ package/mixins/helper.js
@@ -1,32 +1,48 @@
-import { get, toPath, isArray } from "lodash-es";
+function hasOwnProperty(object, property) {
+  return Object.prototype.hasOwnProperty.call(object, property);
+}
+
+function isNumberLike(value) {
+  return String(value).match(/^\d+$/);
+}
+
+function toPath(pathString) {
+  if (Array.isArray(pathString)) {
+    return pathString;
+  }
+  if (typeof pathString === "number") {
+    return [pathString];
+  }
+  pathString = String(pathString);
+
+  // lodash 的实现 - https://github.com/lodash/lodash
+  let pathRx = /[^.[\]]+|\[(?:(-?\d+(?:\.\d+)?)|(["'])((?:(?!\2)[^\\]|\\.)*?)\2)\]|(?=(\.|\[\])(?:\4|$))/g;
+  let pathArray = [];
+
+  pathString.replace(pathRx, (match, number, quote, string) => {
+    pathArray.push(
+      quote ? string : number !== undefined ? Number(number) : match
+    );
+    return pathArray[pathArray.length - 1];
+  });
+  return pathArray;
+}
 
 export default {
   methods: {
-    $deepSet(owner, property, value) {
-      const paths = toPath(property);
-      let ownerCache = owner;
-      let valueCache = null;
+    $deepSet(obj, path, value) {
+      let fields = Array.isArray(path) ? path : toPath(path);
+      let prop = fields.shift();
 
-      paths.forEach((path, index) => {
-        if (index !== paths.length - 1) {
-          const isNumber = /^[0-9]*$/g.test(paths[index + 1]);
-          const currentValue = get(ownerCache, path);
-          valueCache =
-            currentValue === undefined || currentValue === null
-              ? isNumber
-                ? []
-                : {}
-              : currentValue;
-          this.$set(
-            ownerCache,
-            isArray(ownerCache) ? parseInt(path) : path,
-            valueCache
-          );
-          ownerCache = valueCache;
-        } else {
-          this.$set(ownerCache, path, value);
-        }
-      });
+      if (!fields.length) {
+        return this.$set(obj, prop, value);
+      }
+      if (!hasOwnProperty(obj, prop) || obj[prop] === null) {
+        // 当前下标是数字则认定是数组
+        const objVal = fields.length >= 1 && isNumberLike(fields[0]) ? [] : {};
+        this.$set(obj, prop, objVal);
+      }
+      this.$deepSet(obj[prop], fields, value);
     }
   }
 };
