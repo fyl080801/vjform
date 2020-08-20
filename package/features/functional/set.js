ad51d87e456060e3c9d6c541f489aa343fc8e2be
fengyuanliang3 <fengyuanliang2@jd.com>
2020-08-20 13:33:27 +0800

    fix: SET对象则克隆传入值

--- package/features/functional/set.js
+++ package/features/functional/set.js
@@ -1,9 +1,13 @@
 import feature from "../../feature";
 import emitter from "../../utils/emitter";
+import { cloneDeep } from "lodash-es";
 
 feature
   .functional("SET", function(path, value) {
     // 从转换触发的函数不可直接修改model里的值
-    emitter.$emit("update", { path, value });
+    emitter.$emit("update", {
+      path,
+      value: typeof value === "object" ? cloneDeep(value) : value
+    });
   })
   .withDescription("赋值");
