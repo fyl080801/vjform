606574f966cf2c36ac58872450ca620012fdc264
fengyuanliang3 <fengyuanliang2@jd.com>
2020-09-21 15:31:07 +0800

    feat: 去掉SET功能函数，不建议在计算属性或事件里修改model属性值

--- lib/Main.vue
+++ lib/Main.vue
@@ -12,8 +12,6 @@ import renderer from "./renderer";
 import transform from "./features/transform";
 import datasource from "./mixins/datasource";
 import listeners from "./mixins/listeners";
-import emitter from "./utils/emitter";
-import { deepSet } from "./utils/helpers";
 import { v4 } from "uuid";
 
 export default {
@@ -70,17 +68,10 @@ export default {
         field.key = field.key || v4();
         this.attachKey(field.children);
       });
-    },
-    onUpdate({ path, value }) {
-      deepSet(this.data.model, path, value);
     }
   },
   created() {
     this.transform(this.fields);
-    emitter.$on("update", this.onUpdate);
-  },
-  beforeDestroy() {
-    emitter.$off("update", this.onUpdate);
   }
 };
 </script>
